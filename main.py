54eabe0fb2500fcc9224343df3ca8868183c4adc
Даниленко Степан <danilenkostepan@gmail.com>
2026-01-25 16:02:57 +0300

    delete some files

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from site-parser!")
-
-
-if __name__ == "__main__":
-    main()
--- test.py
+++
@@ -1,9 +0,0 @@
-from tqdm import tqdm
-import time
-
-# Было:
-# for i in range(100):
-
-# Стало:
-for i in tqdm(range(100), desc="Обработка файлов"):
-    time.sleep(0.1)  # Имитация работы
