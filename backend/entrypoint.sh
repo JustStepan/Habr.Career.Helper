93c9853e2fc2187f63618d7d22739f35d43aebc0
Даниленко Степан <danilenkostepan@gmail.com>
2026-02-04 22:17:46 +0300

    add favorite card and api/favorite endpoints

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 set -e
 
 echo "🔄 Применение миграций..."
 uv run alembic upgrade head
 
-echo "🚀 Запуск FastAPI с 4 воркерами..."
+echo "🚀 Запуск FastAPI с 1 воркером..."
 exec uv run uvicorn app.main:app \
   --host 0.0.0.0 \
   --port 8000 \
